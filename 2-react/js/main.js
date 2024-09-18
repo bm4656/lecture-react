d5ca93108a99227366f1de72791930ef122465d7
bm4656 <newjin46@gmail.com>
2024-09-18 13:47:34 +0900

    todo: 폼 초기화

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -10,8 +10,26 @@ class App extends React.Component {
     event.preventDefault();
     console.log('TODO: handleSubmit', this.state.searchKeyword);
   }
+
   handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0) {
+      return this.handleReset();
+    }
+
     this.setState({ searchKeyword: event.target.value });
+  }
+
+  handleReset(event) {
+    this.setState(
+      () => {
+        return { searchKeyword: '' };
+      },
+      () => {
+        console.log('TODO: handleReset() -> ', this.state.searchKeyword);
+      }
+    );
   }
 
   render() {
@@ -21,7 +39,9 @@ class App extends React.Component {
           <h2 className='container'>검색</h2>
         </header>
         <div className='container'>
-          <form onSubmit={(event) => this.handleSubmit(event)}>
+          <form
+            onSubmit={(event) => this.handleSubmit(event)}
+            onReset={() => this.handleReset()}>
             <input
               type='text'
               placeholder='검색어를 입력하세요'
