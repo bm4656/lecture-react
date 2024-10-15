2b9abf00aeab8c0279ce0d175fc381c4611682f6
bm4656 <newjin46@gmail.com>
2024-10-15 22:25:24 +0900

    refact: 추천 검색어 탭

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -18,8 +18,13 @@ class App extends React.Component {
       searchResult: [],
       submitted: false,
       selectedTab: TabType.KEYWORD,
-      tabView: store.getKeywordList(),
+      keywordList: [],
     };
+  }
+
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    this.setState({ keywordList });
   }
 
   handleSubmit(event) {
@@ -86,13 +91,16 @@ class App extends React.Component {
         <div className='empty-box'>검색 결과가 없습니다</div>
       );
 
-    const tabView = (
+    const keywordList = (
       <ul className='list'>
-        {this.state.tabView.map((item) => (
-          <li key={item.id}>
-            {item.id}. {item.keyword}
-          </li>
-        ))}
+        {this.state.keywordList.map((item, index) => {
+          return (
+            <li key={item.id}>
+              <span className='number'>{index + 1}</span>
+              <span>{item.keyword}</span>
+            </li>
+          );
+        })}
       </ul>
     );
 
@@ -110,7 +118,10 @@ class App extends React.Component {
             );
           })}
         </ul>
-        {tabView}
+        {this.state.selectedTab === TabType.KEYWORD && keywordList}
+        {this.state.selectedTab === TabType.HISTORY && (
+          <>{`TODO: 최근 검색어`}</>
+        )}
       </>
     );
 
